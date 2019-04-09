f4e1160c70d69931e36f16e2e2b02e887b0f7140
1160300903 <l>
2019-04-09 23:17:39 +0800

    1.write many test cases

--- lexical_analysis.py
+++ lexical_analysis.py
@@ -30,7 +30,7 @@ class Input():
 Keydict = {a[1]:a[0] for a in enumerate(["do","if","else","int","boolean","float","while"],start = 5)}
 booldict = {"true":1,"false":0}#存储属性值不是种别码
 Optiondict = {a[1]:a[0] for a in enumerate(["+","-","*","/","**","++",">","<",">=","<=","==","!=","&&","||","!"],start = 5+len(Keydict))}
-Boundarydict = {a[1]:a[0] for a in enumerate(["(",")","{","}","[","]",";",",","="],start = 5+len(Keydict)+len(Optiondict))}
+Boundarydict = {a[1]:a[0] for a in enumerate(["(",")","{","}","[","]",";","="],start = 5+len(Keydict)+len(Optiondict))}
 class SymbolItem(namedtuple("SymbolItem","name type value")):
     pass
 class SymbolTable():
