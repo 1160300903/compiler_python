62ed1e8c3c8a81f591d5c58fc0db56e083f78f1f
1160300903 <l>
2019-04-18 18:32:45 +0800

    1.fix most bugs

--- grammer_analysis/grammer_analysis.py
+++ grammer_analysis/grammer_analysis.py
@@ -14,6 +14,10 @@ class Queue():
         if len(self.array) == 0:
             return True
         return False
+    def In(self,a):
+        if a in self.array:
+            return True
+        return False
 class Stack():
     def __init__(self):
         self.array = []
@@ -28,8 +32,8 @@ class Stack():
         if len(self.array) == 0:
             return True
         return False
-class grammer_parse():
-    def __init__(self):
+class grammer_parser():
+    def __init__(self,path):
         self.terminators = set()
         self.variable = set()
         self.null_symbol = set("ε")
@@ -41,16 +45,17 @@ class grammer_parse():
         self.goto = []
         self.symbol_stack = None
         self.state_stack = None
-        with open("grammar.txt","r") as g:
+        with open(path,"r") as g:
             g.readline()
             state = 1
             for line in g.readlines():
+                line = line.strip()
                 if line == "Variable":
                     state = 2
-                    break
+                    continue
                 if line == "Expression":
                     state = 3
-                    break
+                    continue
                 if state == 1:
                     for t in line.split():
                         self.terminators.add(t)
@@ -58,10 +63,14 @@ class grammer_parse():
                     self.variable.add(line.split("#")[0])
                 elif state == 3:
                     temp_variable,right_side = line.split("::=")
-                    self.expression.append(Expression(temp_variable,right_side.split()))
+                    self.expression.append(Expression(temp_variable,tuple(right_side.split())))
+        self.first_set_for_char()
+        self.get_all_clourse()
+        self.get_table()
     def first_set_for_char(self):
         for char in self.terminators:
             self.first_dict[char] = set([char])
+        self.first_dict["ε"] = set(["ε"])
         for char in self.variable:
             self.first_dict[char] = set()
         while True:
@@ -74,7 +83,10 @@ class grammer_parse():
                             stable = False
                     elif e.rightside[i] in self.terminators and e.rightside[i] not in self.first_dict[e.leftside]:
                         self.first_dict[e.leftside].add(e.rightside[i])
-                        stable =False
+                        stable = False
+                    elif e.rightside[i] == "ε" and "ε" not in self.first_dict[e.leftside]:
+                        self.first_dict[e.leftside].add("ε")
+                        stable = False
                     if i == len(e.rightside)-1 and "ε" in self.first_dict[e.rightside[i]] and "ε" not in self.first_dict[e.leftside]:
                         self.first_dict[e.leftside].add("ε")
                         stable = False
@@ -83,7 +95,6 @@ class grammer_parse():
             if stable:
                 break
                         
-        
     def first_set_for_string(self,string):
         first = set()
         for i in range(len(string)):
@@ -94,28 +105,40 @@ class grammer_parse():
             if "ε" not in self.first_dict[item]:
                 break
         return first
+    def is_not_specified(self,item):
+        if item.loc_of_point >= len(item.rightside):
+            return False
+        return True
     def get_clourse(self,I):
         clourse_set = set(I)
         while True:
-            stable = True
+            temp_set = set()
             for item in clourse_set:
-                if item.experssion[item.loc_of_point] in self.variable:
+                if self.is_not_specified(item) and item.rightside[item.loc_of_point] in self.variable:
+                    all_char = self.first_set_for_string(item.rightside[item.loc_of_point+1:]+tuple(item.ex_symbol))
                     for e in self.expression:
-                        if e.leftside == item.experssion[item.loc_of_point]:
-                            for c in self.first_set_for_string(item.experssion[item.loc_of_point+1:].append(item.ex_symbol)):
-                                new_item = Item(e.leftside,e.rightside,0,c)
-                                if new_item not in clourse_set:
-                                    clourse_set.add(new_item)
-                                    stable = False
-                            item.experssion.pop()
-            if stable:
+                        if e.leftside == item.rightside[item.loc_of_point]:
+                            if e.rightside == ("ε",):
+                                for c in all_char:
+                                    new_item = Item(e.leftside,e.rightside,1,c)
+                                    if new_item not in clourse_set:
+                                        temp_set.add(new_item)
+                            else:
+                                for c in all_char:
+                                    new_item = Item(e.leftside,e.rightside,0,c)
+                                    if new_item not in clourse_set:
+                                        temp_set.add(new_item)
+            if temp_set:
+                clourse_set = clourse_set | temp_set
+            else:
                 break
         return clourse_set
     def Go(self,I,X):
+        assert X != "ε"
         successive_item_set = set()
         for item in I:
-            if item.experssion[item.loc_of_point] == X:
-                successive_item_set.add(Item(item.rightside,item.leftside,item.loc_of_point+1,item.ex_symbol))
+            if self.is_not_specified(item) and item.rightside[item.loc_of_point] == X:
+                successive_item_set.add(Item(item.leftside,item.rightside,item.loc_of_point+1,item.ex_symbol))
         return self.get_clourse(successive_item_set)
     def get_all_clourse(self):
         queue = Queue()
@@ -126,13 +149,16 @@ class grammer_parse():
             current = queue.dequeue()
             all_clourse.append(current)
             current_index = len(all_clourse)-1
-            self.map_of_clourses[i] = {}
-            for x in (self.terminators | self.variable - set("ε")):
+            self.map_of_clourses[current_index] = {}
+            for x in (self.terminators | self.variable):
                 next = self.Go(current,x)
-                if next != set() and next not in self.all_clourse:
+                if next != set() and next not in self.all_clourse and not queue.In(next):#next不为空，并且不在队列中，也不在all_clourse中
                     queue.enqueue(next)
                     i+=1
-                    self.map_of_clourses[current_index][x] = i   
+                    self.map_of_clourses[current_index][x] = i
+                elif next in self.all_clourse:
+                    self.map_of_clourses[current_index][x] = self.all_clourse.index(next) 
+                elif 
     def find_expression(self,item):
         for i in range(len(self.expression)):
             e = self.expression[i]
@@ -140,22 +166,25 @@ class grammer_parse():
                 return i
         return -1
     def get_table(self):
-        self.action = [{}]*len(self.all_clourse)
-        self.goto = [{}]*len(self.all_clourse)
+        self.action = []
+        self.goto = []
+        for i in range(len(self.all_clourse)):
+            self.action.append({})
+            self.goto.append({})
         for i in range(len(self.all_clourse)):
             for char in self.map_of_clourses[i]:
                 if char in self.terminators:
-                    self.action[char] = self.map_of_clourses[i][char]
+                    self.action[i][char] = self.map_of_clourses[i][char]
                 elif char in self.variable:
-                    self.goto[char] = self.map_of_clourses[i][char]
+                    self.goto[i][char] = self.map_of_clourses[i][char]
             for item in self.all_clourse[i]:
                 if item.loc_of_point == len(item.rightside):
                     index = self.find_expression(item)
                     if index==0 and item.ex_symbol == "#":
                         self.action[i]["#"] = "acc"
                     else:
-                        self.action[i][item.ex_symbol] = "r"+index
-    def parser(self,string):
+                        self.action[i][item.ex_symbol] = "r"+str(index)
+    def parse(self,string):
         self.symbol_stack = Stack()
         self.state_stack = Stack()
         self.symbol_stack.push("#")
@@ -167,14 +196,14 @@ class grammer_parse():
                 top_state = self.state_stack.get_top()
                 char = temp_string[index]
                 command = self.action[top_state][char]
-                if command.isdigit():
+                if command.isdigit():#读入动作
                     self.state_stack.push(command)
                     self.symbol_stack.push(char)
                     index+=1
-                elif command.isalpha():
+                elif command=="acc":#接受动作
                     print("接受")
                     break
-                else:
+                else:#规约动作
                     e = self.expression[int(command[1:])]
                     self.state_stack.pop(len(e.rightside))
                     self.symbol_stack.pop(len(e.rightside))
@@ -183,10 +212,31 @@ class grammer_parse():
                     self.state_stack.push(self.goto[top_state][e.leftside])
         except KeyError as e:
             print("错误")
-            
-                    
-                        
-
-
-
-
+    def show_table(self):
+        sorted_terminators = sorted(list(self.terminators))
+        sorted_variable = sorted(list(self.variable))
+        
+        for i in range(len(self.all_clourse)):
+            print("clourse"+str(i))
+            sorted_expression = sorted(list(self.all_clourse[i]))
+            for item in sorted_expression:
+                temp_list = list(item.rightside)
+                temp_list.insert(item.loc_of_point,".")
+                print(item.leftside+"::="+"".join(temp_list)+"\t"+item.ex_symbol)
+        print("\t"+"\t".join(sorted_terminators)+"\t"+"\t".join(sorted_variable))
+        for i in range(len(self.all_clourse)):
+            print(str(i)+"\t",end="")
+            for char in sorted_terminators:
+                if char in self.action[i]:
+                    print(str(self.action[i][char])+"\t",end="")
+                else:
+                    print("\t",end="")
+            for char in sorted_variable:
+                if char in self.goto[i]:
+                    print(str(self.goto[i][char])+"\t",end="")
+                else:
+                    print("\t",end="")
+            print()
+if __name__ == "__main__":
+    g = grammer_parser("test.txt")
+    g.show_table()
